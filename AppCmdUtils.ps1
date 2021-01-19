1a5b3eae95e7bb48e0abcc84afc0312701cbefdb
Wesley Araujo Vieira <wesley.contato@hotmail.com>
2021-01-19 18:45:10 -0300

    O SwitchArgument AsJob

--- AppCmdUtils.ps1
+++ AppCmdUtils.ps1
@@ -4,14 +4,17 @@ function Add-ApplicationPool {
         [String] $ApplicationPoolName
     )
 
-    $ComputersName | ForEach-Object {
-        $session = New-PSSession -ComputerName $_
-
-        Invoke-Command -Session $session -ScriptBlock {
+    $sessions = $ComputersName | ForEach-Object { New-PSSession -ComputerName $_}
+    $jobs = $sessions | ForEach-Object {
+        Invoke-Command -Session $_ -ScriptBlock {
             "Estamos executando os comandos no computador: $env:COMPUTERNAME"
             $appCmdArgs = "add apppool /name:$(args[0]) /managedRuntimeVersion:v4.0 /managedPipelineMode:Integrated"
             C:Windows\System32\inetsrv\appcmd.exe $appCmdArgs.Split(' ')
 
-        } -ArgumentList $ApplicationPoolName
-    }
+        } -ArgumentList $ApplicationPoolName -AsJob
+    }    
+
+    $jobs | Wait-Job | Select-Object @{Expression = { Receive-Job $_}; Label="Resultado"}, "Name"
+    $jobs | Remove-Job
+    $sessions | Remove-PSSession
 }
