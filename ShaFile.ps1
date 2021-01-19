736447bcf16fb00cb844e158e0ca3bb4081115be
Wesley Araujo Vieira <wesley.contato@hotmail.com>
2021-01-19 15:46:43 -0300

    Início, processamento e fim

--- ShaFile.ps1
+++ ShaFile.ps1
@@ -1,40 +1,40 @@
 function Get-FileSHA1 ($filePath) {
 
-    if ($filePath -ne $null) {
-        $fileContent = Get-Content $filePath
-        $fileBytes = [System.Text.Encoding]::UTF8.GetBytes($fileContent)
-
+    begin {
         $sha1 = New-Object System.Security.Cryptography.SHA1Managed
-
-        $hash = $sha1.ComputeHash($fileBytes)
-
         $prettyHashSB = New-Object System.Text.StringBuilder
+    }
+
+    process {
+
+        $fileContent = Get-Content $filePath
+        $fileBytes = [System.Text.Encoding]::UTF8.GetBytes($fileContent)        
+
+        $hash = $sha1.ComputeHash($fileBytes)        
 
         foreach ($byte in $hash) {
             $hexaNotation = $byte.ToString("X2")
             $prettyHashSB.Append($hexaNotation) | > $null
         }
 
-        $prettyHashSB.ToString()            
-    } else {
-        foreach ($item in $input) {   
-            $fileContent = Get-Content $item
-            $fileBytes = [System.Text.Encoding]::UTF8.GetBytes($fileContent)
-    
-            $sha1 = New-Object System.Security.Cryptography.SHA1Managed
-    
-            $hash = $sha1.ComputeHash($fileBytes)
-    
-            $prettyHashSB = New-Object System.Text.StringBuilder
-    
-            foreach ($byte in $hash) {
-                $hexaNotation = $byte.ToString("X2")
-                $prettyHashSB.Append($hexaNotation) | > $null
-            }
-    
-            $prettyHashSB.ToString()
-        }
+        $prettyHashSB.ToString()
+
+        $prettyHashSB.Clear()
     }
 
+    end {
+        $sha1.Dispose()
+
+    }
+}
+    
+
+function Get-FileSHA256() {
+    
+}
+function Get-FileSHA384() {
+    
+}
+function Get-FileSHA512() {
     
 }
