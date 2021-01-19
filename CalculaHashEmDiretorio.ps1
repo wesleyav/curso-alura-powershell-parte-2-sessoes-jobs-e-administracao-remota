b19a88054efd250274e11889da3ac8df945a96eb
Wesley Araujo Vieira <wesley.contato@hotmail.com>
2021-01-19 14:49:55 -0300

    Conversão de argumentos e o Select a fundo

--- CalculaHashEmDiretorio.ps1
+++ CalculaHashEmDiretorio.ps1
@@ -6,7 +6,7 @@ $ErrorActionPreference = "Stop"
 
 $arquivos = Get-ChildItem $diretorio -File
 
-foreach ($item in $arquivos) {
+foreach ($item in $arquivos | Select-Object FullName) {
     $hashItem = Get-FileSHA1 $item
     Write-Host "O Hash do arquivo $item é $hashItem !"
 }
