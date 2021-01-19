dac8bae5579f7be71a0c5440ae28dffd05ec20f2
Wesley Araujo Vieira <wesley.contato@hotmail.com>
2021-01-19 15:23:37 -0300

    Usando a variável automática input

--- ShaFile.ps1
+++ ShaFile.ps1
@@ -1,18 +1,40 @@
 function Get-FileSHA1 ($filePath) {
-    $fileContent = Get-Content $filePath
-    $fileBytes = [System.Text.Encoding]::UTF8.GetBytes($fileContent)
 
-    $sha1 = New-Object System.Security.Cryptography.SHA1Managed
+    if ($filePath -ne $null) {
+        $fileContent = Get-Content $filePath
+        $fileBytes = [System.Text.Encoding]::UTF8.GetBytes($fileContent)
 
-    $hash = $sha1.ComputeHash($fileBytes)
+        $sha1 = New-Object System.Security.Cryptography.SHA1Managed
 
-    $prettyHashSB = New-Object System.Text.StringBuilder
+        $hash = $sha1.ComputeHash($fileBytes)
 
-    foreach ($byte in $hash) {
-        $hexaNotation = $byte.ToString("X2")
-        $prettyHashSB.Append($hexaNotation) | > $null
+        $prettyHashSB = New-Object System.Text.StringBuilder
+
+        foreach ($byte in $hash) {
+            $hexaNotation = $byte.ToString("X2")
+            $prettyHashSB.Append($hexaNotation) | > $null
+        }
+
+        $prettyHashSB.ToString()            
+    } else {
+        foreach ($item in $input) {   
+            $fileContent = Get-Content $item
+            $fileBytes = [System.Text.Encoding]::UTF8.GetBytes($fileContent)
+    
+            $sha1 = New-Object System.Security.Cryptography.SHA1Managed
+    
+            $hash = $sha1.ComputeHash($fileBytes)
+    
+            $prettyHashSB = New-Object System.Text.StringBuilder
+    
+            foreach ($byte in $hash) {
+                $hexaNotation = $byte.ToString("X2")
+                $prettyHashSB.Append($hexaNotation) | > $null
+            }
+    
+            $prettyHashSB.ToString()
+        }
     }
 
-    $prettyHashSB.ToString()
-
+    
 }
