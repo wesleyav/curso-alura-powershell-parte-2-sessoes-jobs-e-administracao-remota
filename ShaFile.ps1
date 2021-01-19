d3e5c7c1ff6c2598746979d31972cf82c1c25093
Wesley Araujo Vieira <wesley.contato@hotmail.com>
2021-01-19 14:16:22 -0300

    Entendendo o Pipeline de saída

--- ShaFile.ps1
+++ ShaFile.ps1
@@ -4,17 +4,23 @@ function Get-FileSHA1 ($filePath) {
 
     $sha1 = New-Object System.Security.Cryptography.SHA1Managed
 
+    $hash = $sha1.ComputeHash($fileBytes)
+
     $prettyHashSB = New-Object System.Text.StringBuilder
+
     foreach ($byte in $hash) {
         $hexaNotation = $byte.ToString("X2")
-        $prettyHashSB.Append($hexaNotation)
+        $prettyHashSB.Append($hexaNotation) | > $null
     }
 
-    $hash = $sha1.ComputeHash($fileBytes)
-    Write-Host $hash -BackgroundColor Red -ForegroundColor Yellow
+    $prettyHashSB.ToString()
+
 }
 
-Get-FileSHA1 C:\ACELERA DEVS\62-PowerShell parte 2 Sessões, Jobs e Administração Remota\curso-alura-powershell-parte-2-sessoes-jobs-e-administracao-remota\ShaFile.ps1
+$arquivo = "C:\ACELERA DEVS\62-PowerShell parte 2 Sessões, Jobs e Administração Remota\curso-alura-powershell-parte-2-sessoes-jobs-e-administracao-remota\ShaFile.ps1"
+$hashDoArquivo = Get-FileSHA1 $arquivo
+
+Write-Host "O hash do arquivo $arquivo é $hashDoArquivo" -BackgroundColor Red -ForegroundColor Yellow
 
 function Get-FileSHA256() {}
 function Get-FileSHA384() {}
